73a10e9eae76393c308f7c490edbe653b8fb7195
saad7528 <saad0174742@gmail.com>
2026-05-21 19:19:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT ?? 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://SimpleCrudUser:ARJr483VlNFiyjbP@sadasaad.pszei0q.mongodb.net/?appName=SadaSaad`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+const run = async () => {
+    try {
+        await client.connect();
+
+        const db = client.db("simpleCrud");
+        const userCollection = db.collection("users");
+
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Simple crud server is serving')
+});
+
+app.listen(port, () => {
+    console.log(`Crud server is running on port ${port}`);
+});
